e06bc8b2c5ee048f6ffb08ff4ba8c017164ac38d
RPBMedia <rui.palma.baiao@gmail.com>
2023-06-26 22:35:27 +0200

    assets imported. classes in their own file now

---
+++ scripts/classes.js
@@ -0,0 +1,76 @@
+class Sprite {
+    constructor({position,}){
+        this.position = position
+        this.width = 10
+        this.height = 150
+
+    }
+
+    draw() {
+
+    }
+
+    update() {
+        this.draw()
+    }
+}
+
+class Fighter {
+    constructor({position, velocity, color = 'red', offset}){
+        this.position = position
+        this.velocity = velocity
+        this.width = 10
+        this.height = 150
+        this.lastKey
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y,
+            },
+            offset,
+            width: 100,
+            height: 50
+        }
+        this.color = color
+        this.isAttacking = false
+        this.health = 100;
+    }
+
+    draw() {
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+
+        //Draw attack box
+        if(this.isAttacking) {
+            c.fillStyle = 'green'
+            c.fillRect(
+                this.attackBox.position.x,
+                this.attackBox.position.y,
+                this.attackBox.width,
+                this.attackBox.height
+                )
+        }
+    }
+
+    update() {
+        this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        if(this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else {
+            this.velocity.y += gravity
+        }
+    }
+
+    attack(){
+        this.isAttacking = true
+        console.log('attack true')
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100)
+    }
+}
